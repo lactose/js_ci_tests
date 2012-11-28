82e781a2b68acc0eb3837d47e9446d44cd840a3a
zowen <zowen@weblinc.com>
2012-11-28 12:15:49 -0500

    sdfsdaf

--- test/full.js
+++ test/full.js
@@ -1,8 +1,8 @@
-casper.start('http://twitter.github.com/bootstrap/javascript.html#dropdowns', function() {
-    this.test.assertExists('#navbar-example');
-    this.click('#dropdowns .nav-pills .dropdown:last-of-type a.dropdown-toggle');
-    this.waitUntilVisible('#dropdowns .nav-pills .open', function() {
-        this.test.pass('Dropdown is open');
+casper.start('http://local.bhldn.weblinc.com/the-shop-dresses/', function() {
+    this.test.assertExists('#content');
+    this.click('a.wl-outfit-dialog');
+    this.waitUntilVisible('.v-product-quickviewdetailinfo-container', function() {
+        this.test.pass('dialog is open');
     });
 });
 
