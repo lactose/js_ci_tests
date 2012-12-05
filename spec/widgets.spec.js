eb940258b6e1ee869140b6cffb9496c5034a0fa3
zowen <zowen@weblinc.com>
2012-12-05 14:11:30 -0500

    test

---
+++ spec/widgets.spec.js
@@ -0,0 +1,20 @@
+describe("A suite", function() {
+
+    beforeEach(function() {
+        var $newA = $('<a id="testa" class="dialog" href="#testid">Dialog</a>'),
+            $newD = $('<div id="testid">Some content</div>');
+        $('body').append($newD);
+        $('body').append($newA);
+
+    });
+
+    it("contains spec with an expectation", function() {
+        expect(true).toBe(true);
+    });
+
+
+
+
+});
+
+
