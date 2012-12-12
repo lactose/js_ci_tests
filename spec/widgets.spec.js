320064d3d24135326970cdeb15bea771d76d6af7
zowen <zowen@weblinc.com>
2012-12-12 12:02:23 -0500

    test

--- spec/widgets.spec.js
+++ spec/widgets.spec.js
@@ -9,7 +9,7 @@ describe("A suite", function() {
     });
 
     it("contains spec with an expectation", function() {
-        expect(true).toBe(true);
+        expect(false).toBe(true);
     });
 
 
